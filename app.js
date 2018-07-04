790cc285ef18604713dc9fd9f46579a7045ed1b8
KennethJMiddleton <golden.draconic@gmail.com>
2018-07-04 16:04:30 -0400

    displays games in room

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ gameRoom.config(function ($routeProvider) {
             controller: 'landingController'
         })
         .when('/security', {
-            templateURL: 'pages/security.html',
+            templateUrl: 'pages/security.html',
             controller: 'securityController'
         })
         .when('/login', {
@@ -45,6 +45,17 @@ gameRoom.service('credService', function() {
     this.token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiTi9BIiwidXNlcm5hbWUiOiJOL0EiLCJmaXJzdE5hbWUiOiIiLCJsYXN0TmFtZSI6IiIsIm15U2hlbGYiOiJOL0EiLCJmcmllbmRzU2hlbGZzIjpbIiJdfSwiaWF0IjoxMjI5NTM4MDk0LCJleHAiOjEyMjk1NDI2OTQsInN1YiI6Ik4vQSJ9.v13oZp6eZak4qN-6PGlHjWC1J0NLGZ9YnN07-Rdd_vM';
 });
 
+gameRoom.config(function Config($httpProvider, jwtOptionsProvider){
+    jwtOptionsProvider.config({
+        tokenGetter: ['credService', function(credService) {
+          return credService.token;
+        }]
+    })
+    $httpProvider.interceptors.push('jwtInterceptor');
+});
+
+
+
 gameRoom.controller('landingController', ['$scope', function($scope){
 
 }]);
@@ -56,17 +67,16 @@ gameRoom.controller('loginController', ['$scope', '$location', '$resource','jwtH
         $scope.callLoginAPI = $resource('/auth/login');
         var login = new $scope.callLoginAPI({'username': $scope.username, 'password': $scope.password})
         login.$save(function (response) {
-            console.log(response);
             credService.token = response.authToken;
-            console.log(credService.token);
-            console.log(jwtHelper.isTokenExpired(credService.token));
             if (!jwtHelper.isTokenExpired(credService.token)){
                 $location.path('/home');
             };
         }, function (error) {
-            console.log(error);
             if(error.data == 'Unauthorized'){
                 $scope.loginError = 'That username and password combination are not in our system. Please try again.'
+            }
+            else{
+                $scope.loginError = 'Something went wrong! Make sure you typed something in both fields. If you did, please reload the page and try again.'
             };
         })
     };
@@ -77,15 +87,67 @@ gameRoom.controller('newAccountController', ['$scope', function($scope){
 }]);
 
 gameRoom.controller('homeController', ['$scope','$location', 'jwtHelper', 'credService', function($scope,$location,jwtHelper,credService){
-    /*if(jwtHelper.isTokenExpired(credService.token)){
-        console.log('it works!')
+    if(jwtHelper.isTokenExpired(credService.token)){
         $location.path('/security');
-    }*/
+    }
 
 }]);
 
-gameRoom.controller('myGamesController', ['$scope', function($scope){
-
+gameRoom.controller('myGamesController', ['$scope', '$location', '$resource', 'jwtHelper', 'credService', function($scope, $location, $resource, jwtHelper, credService){
+    if(jwtHelper.isTokenExpired(credService.token)){
+        $location.path('/security');
+    }
+    $scope.table = "You don't have any games on your shelf yet. You should click on 'Search' above and fix that."
+    var payload = jwtHelper.decodeToken(credService.token);
+    $scope.getGames = $resource('/gameshelf/' + payload.user.myShelf);
+    $scope.getGames.query(function(games){
+        console.log(games);
+        if (games.length === 0){
+            $scope.emptyRoom = true;
+        }
+        else {
+            $scope.gamelist = Object.values(games);
+            $scope.gamelist.splice(-2,2);
+            console.log($scope.gamelist);
+            for(var game, i=0; game=$scope.gamelist[i++];){
+                var qualityArray = [];
+                if(game.bluffing === true ){
+                    qualityArray.push("bluffing");
+                }
+                if(game.coop === true ){
+                    qualityArray.push("cooperative");
+                }
+                if(game.deckBuilding === true ){
+                    qualityArray.push("deck building");
+                }
+                if(game.dice === true ){
+                    qualityArray.push("dice");
+                }
+                if(game.party === true ){
+                    qualityArray.push("party");
+                }
+                if(game.setCollecting === true ){
+                    qualityArray.push("set collecting");
+                }
+                if(game.tokenMovement === true ){
+                    qualityArray.push("token movement");
+                }
+                if(game.tokenPlacement === true ){
+                    qualityArray.push("token placement");
+                }
+                if(game.trivia === true ){
+                    qualityArray.push("trivia");
+                }
+                if(game.expansion === true ){
+                    qualityArray.push("expansion");
+                }
+                game.qualities = qualityArray.toString();
+            }
+            console.log($scope.gamelist);
+        } 
+    },function(error){
+        console.log(error);
+    });
 }]);
 
 gameRoom.controller('searchController', ['$scope', function($scope){
